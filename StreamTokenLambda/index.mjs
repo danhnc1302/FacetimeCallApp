b442eb8a80d4eae5620c7ee36c9faa65d1e844c3
danhnc <danhnc1302@gmail.com>
2024-05-01 22:00:17 +0700

    Stream Token Lambda

---
+++ StreamTokenLambda/index.mjs
@@ -0,0 +1,62 @@
+import { StreamChat } from 'stream-chat';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = process.env.SUPABASE_URL ?? 'https://wmaslehxmzwklcgyozro.supabase.co';
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtYXNsZWh4bXp3a2xjZ3lvenJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0MTEzNTAsImV4cCI6MjAyOTk4NzM1MH0.iY6QNB3QjLWNVhKmhbhBCn-EcQlXZI7akB_jAKzCqaA';
+const STREAM_API_KEY = process.env.STREAM_API_KEY ?? '68yr8cv2k5b6';
+const STREAM_API_SECRET = process.env.STREAM_API_SECRET ?? 'autypwyh7kn6w5u65zkedbvgdr2k7s63mva93wg6zfevjxmnurgkfwb629adeguh';
+
+// Initialize a Server Client
+const serverClient = StreamChat.getInstance(STREAM_API_KEY, STREAM_API_SECRET);
+
+export const handler = async (event) => {
+  const authToken = event.queryStringParameters?.token;
+
+  if (!authToken) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Auth token not provided' }),
+    };
+  }
+
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      SUPABASE_URL,
+      SUPABASE_ANON_KEY,
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: `Bearer ${authToken}` } } }
+    );
+
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser();
+
+    if (!user) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'User not found' }),
+      };
+    }
+
+    console.log(user)
+
+    // Create User Token
+    const token = serverClient.createToken(user.id);
+
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ token }),
+    };
+    return response;
+  } catch (e) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unexpected Errro' }),
+    };
+  }
+};
+
